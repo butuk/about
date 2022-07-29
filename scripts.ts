3b961b56382b18f845255d19226f6d47a6c42785
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-07-29 19:55:05 +0300

    feat: end typescript training

--- scripts.ts
+++ scripts.ts
@@ -1,19 +1,6 @@
-// Welcome to the TypeScript Playground, this is a website
-// which gives you a chance to write, share and learn TypeScript.
-
-// You could think of it in three ways:
-//
-//  - A location to learn TypeScript where nothing can break
-//  - A place to experiment with TypeScript syntax, and share the URLs with others
-//  - A sandbox to experiment with different compiler features of TypeScript
-
-const anExampleVariable = "Hello World"
-console.log(anExampleVariable)
-
-// To learn more about the language, click above in "Examples" or "What's New".
-// Otherwise, get started by removing these comments and the world is your playground.
-function addNumbers(x:number, y:number) {
-    return x + y;
-}
-
-console.log(addNumbers(3, 6));
+const trainer = document.querySelector('.trainer');
+const trained = document.querySelector('.trained');
+trainer.addEventListener('click', ()=> {
+    trainer.classList.add('hidden');
+    trained.classList.remove('hidden');
+})
