74670e3fbcf5825625bf7032dfab2b32569845b9
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-08-01 22:43:36 +0300

    feat: make headline clickable

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
 "use strict";
+const reset = document.querySelector('.reset');
 const trainer = document.querySelector('.trainer');
+const graduated = document.querySelector('.graduated');
+const ux = document.querySelector('.ux-ui');
+const help = document.querySelector('.can-help');
+const headline = document.querySelector('.headline');
 //const trained = document.querySelector('.trained');
+ux.addEventListener('click', () => {
+    help.classList.remove('hidden');
+    ux.classList.add('hidden');
+    reset.classList.remove('hidden');
+    headline.classList.add('hidden');
+});
 trainer.addEventListener('click', () => {
+    graduated.classList.remove('hidden');
+    trainer.classList.add('hidden');
+    reset.classList.remove('hidden');
+    headline.classList.add('hidden');
+});
+reset.addEventListener('click', () => {
+    help.classList.add('hidden');
+    ux.classList.remove('hidden');
+    graduated.classList.add('hidden');
+    trainer.classList.remove('hidden');
+    reset.classList.add('hidden');
+    headline.classList.remove('hidden');
 });
 // Dynamic favicon
 const iconPlace = document.getElementById('favicon');
