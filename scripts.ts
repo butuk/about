ef74a3219243213545d4d8797eca8ce81ff40a78
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-08-03 00:19:47 +0300

    feat: add animation to appearing blocks

--- scripts.ts
+++ scripts.ts
@@ -4,26 +4,27 @@ const graduated = document.querySelector('.graduated');
 const ux = document.querySelector('.ux-ui');
 const help = document.querySelector('.can-help');
 const headline = document.querySelector('.headline');
-//const trained = document.querySelector('.trained');
-ux.addEventListener('click', () => {
-    help.classList.remove('hidden');
-    ux.classList.add('hidden');
-    reset.classList.remove('hidden');
-    headline.classList.add('hidden');
-});
-trainer.addEventListener('click', ()=> {
-    graduated.classList.remove('hidden');
-    trainer.classList.add('hidden');
-    reset.classList.remove('hidden');
-    headline.classList.add('hidden');
-});
+
+const clickable = document.querySelectorAll('.clickable');
+
+clickable.forEach(element => {
+    element.addEventListener('click', () => {
+        const nextBlock = element.nextElementSibling;
+        nextBlock.classList.remove('hidden');
+        nextBlock.style.animation = 'expand 300ms ease';
+        reset.classList.remove('hidden');
+        element.classList.add('hidden');
+        headline.classList.add('hidden');
+    })
+})
+
 reset.addEventListener('click', () => {
-    help.classList.add('hidden');
-    ux.classList.remove('hidden');
-    graduated.classList.add('hidden');
-    trainer.classList.remove('hidden');
-    reset.classList.add('hidden');
     headline.classList.remove('hidden');
+    ux.classList.remove('hidden');
+    trainer.classList.remove('hidden');
+    help.classList.add('hidden');
+    graduated.classList.add('hidden');
+    reset.classList.add('hidden');
 })
 
 
