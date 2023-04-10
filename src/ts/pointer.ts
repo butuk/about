48b5aa8e802cc0fcc8851d5a5eb67d321082c503
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-04-10 22:49:15 +0200

    feat: make headline pointer rotate

--- src/ts/pointer.ts
+++ src/ts/pointer.ts
@@ -6,7 +6,28 @@ export class Pointer {
         document.body.prepend(pointer);
 
         pointer.addEventListener('click', function () {
-            window.scrollTo(0,0);
-        })
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth',
+            });
+        });
+
+        let lastScroll = window.scrollY;
+        const header = document.querySelector('.headline');
+
+
+        window.addEventListener('scroll', function () {
+            const currentScroll = window.scrollY;
+            let angle = header ? (header.getBoundingClientRect().top - pointer.getBoundingClientRect().top) / 2 : null;
+            if (angle && angle <= -90) {
+                angle = -90;
+            };
+            console.log(angle);
+            const rotation = angle ? (currentScroll > lastScroll ? angle : -angle) : null;
+            pointer.style.transform = `rotate(${rotation}deg)`;
+        });
     }
 }
+
+
